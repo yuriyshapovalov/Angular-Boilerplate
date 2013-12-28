4348ea4eeb82d8db9a8ecbe83c2fea5b5eb32d06
Yuriy <shapovalov.yuri@gmail.com>
2013-12-28 14:03:32 -0900

    Added routing to config and default templates

--- app/app.js
+++ app/app.js
@@ -1,2 +1,15 @@
 var app = angular.module('app', [])
-    .config()
+
+app.config(['$routeProvider',
+	function($routeProvider) {
+		$routeProvider
+			.when('/', {
+				templateUrl: 'index.tpl',
+				controller: 'IndexCtrl'})
+			.when('/contacts', {
+				templateUrl: 'contacts.tpl',
+				controller: 'ContactsCtrl'})
+			.otherwise({
+				templateUrl: 'notFound404.tpl',
+				controller: 'Error404Ctrl'})
+	}]);
