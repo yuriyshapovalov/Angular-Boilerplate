7ad80abe2824bd84721dc047eebf3692a94e9fb8
Yuriy Shapovalov <shapovalov.yuri@gmail.com>
2013-12-29 20:15:17 +0200

    Added minor changes

--- app/app.js
+++ app/app.js
@@ -1,5 +1,23 @@
+<<<<<<< HEAD
 angular.module('app', [''])
     .config(['$routeProvider'], 
         function($routeProvider){
         
         }]);
+=======
+var app = angular.module('app', [])
+
+app.config(['$routeProvider',
+	function($routeProvider) {
+		$routeProvider
+			.when('/', {
+				templateUrl: 'index.tpl',
+				controller: 'IndexCtrl'})
+			.when('/contacts', {
+				templateUrl: 'contacts.tpl',
+				controller: 'ContactsCtrl'})
+			.otherwise({
+				templateUrl: 'notFound404.tpl',
+				controller: 'Error404Ctrl'})
+	}]);
+>>>>>>> 4348ea4eeb82d8db9a8ecbe83c2fea5b5eb32d06
