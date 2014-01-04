e2cf59ced9dced947c9c16c84df7c2b62f9969d8
Yuriy Shapovalov <shapovalov.yuri@gmail.com>
2014-01-05 00:32:46 +0200

    Complete grunt setup for auto test and lint runners

--- gruntfile.js
+++ gruntfile.js
@@ -3,17 +3,32 @@ module.exports = function(grunt) {
     grunt.initConfig({
         jasmine: {
             pivotal: {
-                src: 'app/js/**/.js',
+                src: 'app/js/**/*.js',
                 options: {
                     specs: 'test/test.js',
                     vendor: 'lib/*.js'
                 }
             }
+        },
+
+        jshint: {
+            all: ['app/js/**/*.js'],
+            options: {
+                curly: true
+            }
+        },
+
+        watch: {
+            files: ['app/js/**/*.js'],
+            tasks: ['jshint', 'jasmine']
         }
     
     });
 
     grunt.loadNpmTasks('grunt-contrib-jasmine');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-watch');
 
-    grunt.registerTask('default', ['jasmine']);
+
+    grunt.registerTask('default', ['jasmine', 'jshint']);
 }
