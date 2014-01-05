e663d87310a5a7172db317d2776b1058581a564b
Yuriy Shapovalov <shapovalov.yuri@gmail.com>
2014-01-05 18:44:24 +0200

    Added npm batch installer

--- setup.bat
+++ setup.bat
@@ -1,6 +1,12 @@
-@ECHO OFF
-
-set path_to_mongodb = 'C:\mongodb\bin'
+npm install phantomjs
+npm install karma
+npm install grunt_cli -g
+npm install grunt_cli
+npm install grunt-contrib-jasmine --save-dev
+REM npm install grunt-contrib-qunit --save-dev
+REM npm install grunt-contrib-mocha --save-dev
+npm install grunt-contrib-jshint --save-dev
+npm install grunt-contrib-watch --save-dev
 
 
 
